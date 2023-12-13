da0702906416779a3618fcba65af7b0e39ec6f31
Tomáš Prusenovský <tomasp0303@gmial.com>
2023-12-13 17:22:19 +0100

    Initial Commit

---
+++ src/BitScrib.cpp
@@ -0,0 +1,12 @@
+﻿// BitScrib.cpp : Defines the entry point for the application.
+//
+
+#include "BitScrib.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
